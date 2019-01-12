d55023276bcc8714abd270385ab9cba2b8eb8ee6
Ahren-Li <lili@vamrs.com>
2019-01-12 11:56:17 +0800

    add staruml logo
    Signed-off-by: Ahren-Li <lili@vamrs.com>

--- staruml/install.sh
+++ staruml/install.sh
@@ -2,5 +2,7 @@
 
 FILE_WGET "http://staruml.io/download/releases/StarUML-3.0.2-x86_64.AppImage" \
 		  "Staruml" "${OUTPUT}/Staruml/staruml" "software/staruml/md5" "${HTTP_PROXY}"
-
-INSTALL_ICON "${SOFTWARE_STARUML_DESKTOP_PATH}" "${HOME}/Desktop/" "${OUTPUT}/Staruml/staruml" "${OUTPUT}/Staruml/staruml"
+if [ "0" == "$?" ];then
+	cp software/staruml/staruml_logo.png ${OUTPUT}/Staruml/
+	INSTALL_ICON "${SOFTWARE_STARUML_DESKTOP_PATH}" "${HOME}/Desktop/" "${OUTPUT}/Staruml/staruml_logo.png" "${OUTPUT}/Staruml/staruml"
+fi
